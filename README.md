01819c7b9277803e13da3964dacd912819ff9772
SB1019 <sasrinath@ucsd.edu>
2024-05-27 21:41:30 -0700

    Completed lab

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Lab8-Starter
+
+This Lab contains the submission for **Samvrit Srinath** for Lab 8: Fetch API's and Service Workers. 
+
+The deployed site can be found at: [https://samvritsrinath.github.io/Lab8-Starter/index.html](https://samvritsrinath.github.io/Lab8-Starter/index.html)
+
+## Service Workers and Graceful Degradation
+
+Service Workers and Graceful Degredation are correlated due to the fact that Service Workers can be used to cache the contents of a website and provide them even when a user is offline. A key tenet of Graceful Degredation is to offer the highest level of functionality to users, when the User Environment supports it. In this case, we are considering whether or not the user is online or offline. Regardless of network conenctivity, we **degrade** the user experience by providing a cached version of the website. This is where Service Workers come in to provide this cached resource. While the full functionality of the app cannot be experienced offline, the user can still access the website and view the cached content and navigate through the website. This allows the user to engage with the service regardless of their network connectivity. Thus, Web Apps can be designed to gracefully degrade in the absence of network connectivity by using Service Workers to cache the website's content. And we implement this custom service worker in the `sw.js` file, where we fetch the resources in `RECIPE_URLS` and cache them for future use. 
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -45,6 +45,18 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
+  if('serviceWorker' in navigator){
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('./sw.js')
+      .then((registration) => {
+        console.log('ServiceWorker registration successful', registration);
+      })
+      .catch((error) => {
+        console.error('ServiceWorker registration failed', error);
+      });
+    
+    });
+  }
   // B2. TODO - Listen for the 'load' event on the window object.
   // Steps B3-B6 will be *inside* the event listener's function created in B2
   // B3. TODO - Register './sw.js' as a service worker (The MDN article
@@ -69,20 +81,44 @@ async function getRecipes() {
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
   /**************************/
+  const recipes = localStorage.getItem('recipes');
+  if (recipes) { //checks for Null
+    return JSON.parse(recipes);
+  }
   // The rest of this method will be concerned with requesting the recipes
   // from the network
   // A2. TODO - Create an empty array to hold the recipes that you will fetch
+  const fetchedRecipes = [];
   // A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
   //            has a great article on them. A promise takes one parameter - A
   //            function (we call these callback functions). That function will
   //            take two parameters - resolve, and reject. These are functions
   //            you can call to either resolve the Promise or Reject it.
   /**************************/
+  return new Promise(async (resolve, reject) => {
   // A4-A11 will all be *inside* the callback function we passed to the Promise
   // we're returning
   /**************************/
   // A4. TODO - Loop through each recipe in the RECIPE_URLS array constant
   //            declared above
+    for(const recipe_url of RECIPE_URLS){
+      try {
+        // console.log(recipe_url);
+        // console.log(`Fetching recipe from: ${recipe_url}`); debugging statement
+        const response = await fetch(recipe_url);
+        const recipe_json = await response.json();
+
+        fetchedRecipes.push(recipe_json);
+        //validation to see if all recipes have been fetched
+        if(fetchedRecipes.length === RECIPE_URLS.length){
+          saveRecipesToStorage(fetchedRecipes);
+          resolve(fetchedRecipes);
+        }
+      } catch (error) {
+        console.error(error);
+        reject(error);
+      }
+    }
   // A5. TODO - Since we are going to be dealing with asynchronous code, create
   //            a try / catch block. A6-A9 will be in the try portion, A10-A11
   //            will be in the catch portion.
@@ -98,6 +134,12 @@ async function getRecipes() {
   //            if you have, then save the recipes to storage using the function
   //            we have provided. Then, pass the recipes array to the Promise's
   //            resolve() method.
+  //didn't work if I kept it here
+  // if(fetchedRecipes.length === RECIPE_URLS.length){
+  //   saveRecipesToStorage(fetchedRecipes);
+  //   resolve(fetchedRecipes);
+  // }
+  });
   // A10. TODO - Log any errors from catch using console.error
   // A11. TODO - Pass any errors to the Promise's reject() function
 }
